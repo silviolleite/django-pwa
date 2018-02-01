25c6952cde3e6abe9059bc0bf80de65c06cfbcba
Scott Vitale <svvitale@gmail.com>
2018-01-31 21:33:50 -0700

    Move to pipenv and bump revision to 0.1.1 to fix "Site cannot be installed: the page does not work offline" error.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-progressive-web-app',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
