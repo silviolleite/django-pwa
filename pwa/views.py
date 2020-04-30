8534e6a749aa536b687724d1b61f82e11ccddd03
Vivien <VivienGiraud@users.noreply.github.com>
2020-05-01 00:57:42 +0200

    Returns correct content_type

--- pwa/views.py
+++ pwa/views.py
@@ -14,7 +14,7 @@ def manifest(request):
         setting_name: getattr(app_settings, setting_name)
         for setting_name in dir(app_settings)
         if setting_name.startswith('PWA_')
-    })
+    }, content_type='application/json')
 
 
 def offline(request):
