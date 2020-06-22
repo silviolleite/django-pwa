991a6ee8c27a0e19c94d5a2cb3963dbf3632c5ab
Sahil Nirkhe <sahilnirkhe@outlook.com>
2020-06-22 21:57:09 +0530

    Escape wildcard in paths
    Both serviceworker.js and manifest.json load even if the '.' character is replaced by any other character. This regex fix will prevent that.

--- pwa/urls.py
+++ pwa/urls.py
@@ -4,7 +4,7 @@ from .views import manifest, service_worker, offline
 
 # Serve up serviceworker.js and manifest.json at the root
 urlpatterns = [
-    url('^serviceworker.js$', service_worker, name='serviceworker'),
-    url('^manifest.json$', manifest, name='manifest'),
+    url(r'^serviceworker\.js$', service_worker, name='serviceworker'),
+    url(r'^manifest\.json$', manifest, name='manifest'),
     url('^offline/$', offline, name='offline')
 ]
