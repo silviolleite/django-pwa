dbfec5f21178ce0da2ff747ff40a34f89caaf29a
kygoh <gohkengyew@gmail.com>
2021-02-28 14:50:45 +0000

    Fix manifest URL if app is not mounted in root of webserver

--- pwa/app_settings.py
+++ pwa/app_settings.py
@@ -14,6 +14,9 @@ _PWA_SCRIPT_PREFIX = get_script_prefix()
 # Path to the service worker script that will be registered
 PWA_SERVICE_WORKER_SCRIPT_URL = _PWA_SCRIPT_PREFIX + 'serviceworker.js'
 
+# Path to the manifest
+PWA_MANIFEST_URL = _PWA_SCRIPT_PREFIX + 'manifest.json'
+
 # Path to the service worker implementation.  Default implementation is empty.
 PWA_SERVICE_WORKER_PATH = getattr(settings, 'PWA_SERVICE_WORKER_PATH',
                                   os.path.join(os.path.abspath(os.path.dirname(__file__)), 'templates',
