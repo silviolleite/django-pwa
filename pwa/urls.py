e25c64a8b5a9faaff9302bc80fb54da9d7287932
Salim Dason <dasonsalim@outlook.com>
2021-12-11 19:52:40 +0100

    modified pwa/urls.py to use re_path to solve django.url import error reported by some users

--- pwa/urls.py
+++ pwa/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from .views import manifest, service_worker, offline
 
 # Serve up serviceworker.js and manifest.json at the root
 urlpatterns = [
-    url(r'^serviceworker\.js$', service_worker, name='serviceworker'),
-    url(r'^manifest\.json$', manifest, name='manifest'),
-    url('^offline/$', offline, name='offline')
+    re_path(r'^serviceworker\.js$', service_worker, name='serviceworker'),
+    re_path(r'^manifest\.json$', manifest, name='manifest'),
+    re_path('^offline/$', offline, name='offline')
 ]
