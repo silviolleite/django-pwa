6c585fe72041188e32747c1b1bfdb8e53197a508
Petr Dlouhý <petr.dlouhy@email.cz>
2021-12-16 14:31:02 +0100

    fix for Django 4.0

--- pwa/urls.py
+++ pwa/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 
 from .views import manifest, service_worker, offline
 
 # Serve up serviceworker.js and manifest.json at the root
 urlpatterns = [
-    url(r'^serviceworker\.js$', service_worker, name='serviceworker'),
-    url(r'^manifest\.json$', manifest, name='manifest'),
-    url('^offline/$', offline, name='offline')
+    path('serviceworker.js', service_worker, name='serviceworker'),
+    path('manifest.json', manifest, name='manifest'),
+    path('offline/', offline, name='offline')
 ]
