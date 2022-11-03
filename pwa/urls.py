5131b8a65159fbb137af25582f78d0bda6ddcd27
cr0hn <cr0hn@cr0hn.com>
2022-11-03 14:42:54 +0100

    Update urls.py
    Add compatibility with Django 4.0, maintaining the compatibility for older Django versions

--- pwa/urls.py
+++ pwa/urls.py
@@ -1,10 +1,23 @@
-from django.urls import re_path
+from django import VERSION
+
 
 from .views import manifest, service_worker, offline
 
-# Serve up serviceworker.js and manifest.json at the root
-urlpatterns = [
-    re_path(r'^serviceworker\.js$', service_worker, name='serviceworker'),
-    re_path(r'^manifest\.json$', manifest, name='manifest'),
-    re_path('^offline/$', offline, name='offline')
-]
+if VERSION >= (4, 0):
+    from django.urls import path
+
+    urlpatterns = [
+        path('serviceworker.js', service_worker, name='serviceworker'),
+        path('manifest.json', manifest, name='manifest'),
+        path('offline/', offline, name='offline')
+    ]
+
+else:
+    from django.urls import re_path
+
+    # Serve up serviceworker.js and manifest.json at the root
+    urlpatterns = [
+        re_path(r'^serviceworker\.js$', service_worker, name='serviceworker'),
+        re_path(r'^manifest\.json$', manifest, name='manifest'),
+        re_path('^offline/$', offline, name='offline')
+    ]
