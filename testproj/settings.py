a4d9282a2f2dba9fa93111349d64d4cb0c8480b8
Christian Hartung <hartung@live.com>
2024-11-20 11:35:52 -0300

    tests: use pytest

---
+++ testproj/settings.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = "eo#$u+kt4h_gx@ag*3am%42hq^8or2ycumka%9vd&(z9+2s7rz"  # noqa: S105
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "pwa",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "testproj.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "testproj.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
+
+
+STATIC_URL = "static/"
+STATIC_ROOT = "static-files/"
--- tests/test_template_tag_meta.py
+++ tests/test_template_tag_meta.py
@@ -1,64 +1,67 @@
+import pytest
 from django.template import Context, Template
-from django.test import TestCase
+from pytest_django.asserts import assertInHTML
 
 
-class CreateMetaTemplateTagTest(TestCase):
-    def setUp(self):
-        context = Context({})
-        template_to_render = Template("{% load pwa %}" "{% progressive_web_app_meta %}")
-        self.rendered_template = template_to_render.render(context)
+@pytest.mark.parametrize(
+    "tag",
+    [
+        '<link rel="apple-touch-icon" href="/static/images/icons/icon-72x72.png" sizes="72x72">',
+        '<link rel="apple-touch-icon" href="/static/images/icons/icon-96x96.png" sizes="96x96">',
+        '<link rel="apple-touch-icon" href="/static/images/icons/icon-128x128.png" sizes="128x128">',
+        '<link rel="apple-touch-icon" href="/static/images/icons/icon-144x144.png" sizes="144x144">',
+        '<link rel="apple-touch-icon" href="/static/images/icons/icon-152x152.png" sizes="152x152">',
+        '<link rel="apple-touch-icon" href="/static/images/icons/icon-192x192.png" sizes="192x192">',
+        '<link rel="apple-touch-icon" href="/static/images/icons/icon-384x384.png" sizes="384x384">',
+        '<link rel="apple-touch-icon" href="/static/images/icons/icon-512x512.png" sizes="512x512">',
+        '<link rel="manifest" href="/manifest.json">',
+        '<meta name="mobile-web-app-capable" content="yes">',
+        '<meta name="theme-color" content="#000">',
+        '<meta name="apple-mobile-web-app-capable" content="yes">',
+        '<meta name="apple-mobile-web-app-title" content="MyApp">',
+        '<meta name="application-name" content="MyApp">',
+        '<meta name="apple-mobile-web-app-status-bar-style" content="default">',
+        '<link rel="icon" sizes="512x512" href="/static/images/icons/icon-512x512.png">',
+        '<link href="/static/images/icons/splash-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>',
+        '<link href="/static/images/icons/splash-750x1334.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>',
+        '<link href="/static/images/icons/splash-1242x2208.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image"/>',
+        '<link href="/static/images/icons/splash-1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image"/>',
+        '<link href="/static/images/icons/splash-828x1792.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>',
+        '<link href="/static/images/icons/splash-1242x2688.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image"/>',
+        '<link href="/static/images/icons/splash-1536x2048.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>',
+        '<link href="/static/images/icons/splash-1668x2224.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>',
+        '<link href="/static/images/icons/splash-1668x2388.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>',
+        '<link href="/static/images/icons/splash-2048x2732.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>',
+        '<meta name="msapplication-TileColor" content="#fff">',
+        '<meta name="msapplication-TileImage" content="/static/images/icons/icon-512x512.png">',
+    ],
+)
+def test_has_tags(tag):
+    context = Context({})
+    template_to_render = Template("{% load pwa %}" "{% progressive_web_app_meta %}")
+    rendered_template = template_to_render.render(context)
 
-    def test_has_tags(self):
-        """Must contains the tags in HTML"""
-        tags = [
-            '<link rel="apple-touch-icon" href="/static/images/icons/icon-72x72.png" sizes="72x72">',
-            '<link rel="apple-touch-icon" href="/static/images/icons/icon-96x96.png" sizes="96x96">',
-            '<link rel="apple-touch-icon" href="/static/images/icons/icon-128x128.png" sizes="128x128">',
-            '<link rel="apple-touch-icon" href="/static/images/icons/icon-144x144.png" sizes="144x144">',
-            '<link rel="apple-touch-icon" href="/static/images/icons/icon-152x152.png" sizes="152x152">',
-            '<link rel="apple-touch-icon" href="/static/images/icons/icon-192x192.png" sizes="192x192">',
-            '<link rel="apple-touch-icon" href="/static/images/icons/icon-384x384.png" sizes="384x384">',
-            '<link rel="apple-touch-icon" href="/static/images/icons/icon-512x512.png" sizes="512x512">',
-            '<link rel="manifest" href="/manifest.json">',
-            '<meta name="mobile-web-app-capable" content="yes">',
-            '<meta name="theme-color" content="#000">',
-            '<meta name="apple-mobile-web-app-capable" content="yes">',
-            '<meta name="apple-mobile-web-app-title" content="MyApp">',
-            '<meta name="application-name" content="MyApp">',
-            '<meta name="apple-mobile-web-app-status-bar-style" content="default">',
-            '<link rel="icon" sizes="512x512" href="/static/images/icons/icon-512x512.png">',
-            '<link href="/static/images/icons/splash-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>',
-            '<link href="/static/images/icons/splash-750x1334.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>',
-            '<link href="/static/images/icons/splash-1242x2208.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image"/>',
-            '<link href="/static/images/icons/splash-1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image"/>',
-            '<link href="/static/images/icons/splash-828x1792.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>',
-            '<link href="/static/images/icons/splash-1242x2688.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image"/>',
-            '<link href="/static/images/icons/splash-1536x2048.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>',
-            '<link href="/static/images/icons/splash-1668x2224.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>',
-            '<link href="/static/images/icons/splash-1668x2388.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>',
-            '<link href="/static/images/icons/splash-2048x2732.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>',
-            '<meta name="msapplication-TileColor" content="#fff">',
-            '<meta name="msapplication-TileImage" content="/static/images/icons/icon-512x512.png">',
-        ]
-        for text in tags:
-            with self.subTest():
-                self.assertInHTML(text, self.rendered_template)
+    assertInHTML(tag, rendered_template)
 
-    def test_has_serviceworker(self):
-        """Must have the script tag with serviceworker registration"""
-        contents = [
-            '<script type="text/javascript">',
-            "if ('serviceWorker' in navigator) {",
-            "navigator.serviceWorker.register('/serviceworker.js', {",
-            "scope: '/'",
-            "}).then(function (registration) {",
-            "console.log('django-pwa: ServiceWorker registration successful with scope: ', registration.scope);",
-            "}, function (err) {",
-            "console.log('django-pwa: ServiceWorker registration failed: ', err);",
-            "});",
-            "</script>",
-        ]
 
-        for expected in contents:
-            with self.subTest():
-                self.assertIn(expected, self.rendered_template)
+@pytest.mark.parametrize(
+    "content",
+    [
+        '<script type="text/javascript">',
+        "if ('serviceWorker' in navigator) {",
+        "navigator.serviceWorker.register('/serviceworker.js', {",
+        "scope: '/'",
+        "}).then(function (registration) {",
+        "console.log('django-pwa: ServiceWorker registration successful with scope: ', registration.scope);",
+        "}, function (err) {",
+        "console.log('django-pwa: ServiceWorker registration failed: ', err);",
+        "});",
+        "</script>",
+    ],
+)
+def test_assert_has_serviceworker(content):
+    context = Context({})
+    template_to_render = Template("{% load pwa %}" "{% progressive_web_app_meta %}")
+    rendered_template = template_to_render.render(context)
+
+    assert content in rendered_template
