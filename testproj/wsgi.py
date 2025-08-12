25cdbcf8bb319ccc410300db11d18312b8101238
Christian Hartung <hartung@live.com>
2025-08-12 06:40:10 -0300

    feat: improve test project (#133)
    * feat: improve test project
    
    * build: update ubuntu version on test action

--- testproj/wsgi.py
+++ testproj/wsgi.py
@@ -1,7 +1,7 @@
 import os
 
-from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproj.settings")
 
-application = get_asgi_application()
+application = get_wsgi_application()
