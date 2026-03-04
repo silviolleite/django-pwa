f97feb40f4801998d02b64e26b112a65c27d4e86
Christian Hartung <6785871+hartungstenio@users.noreply.github.com>
2026-03-03 22:33:02 -0300

    Project update (#141)
    * chore: update python/django compatibility
    
    * chore: update linters
    
    * chore: use pre-commit/deptry from default environment
    
    * chore: use hatch for testing
    
    * chore: PEP-639 compliance

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproj.settings")
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line  # noqa: PLC0415
     except ImportError as exc:
         msg = (
             "Couldn't import Django. Are you sure it's installed and "
