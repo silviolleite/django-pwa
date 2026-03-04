85608ad1a5ba57303e114bffb1626e4de88e0bcf
Christian Hartung <6785871+hartungstenio@users.noreply.github.com>
2026-03-03 22:00:35 -0300

    chore: update linters

--- src/pwa/urls.py
+++ src/pwa/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path, re_path
+from django.urls import path
 
 from .views import manifest, offline, service_worker
 
 app_name = "pwa"
 
 urlpatterns = [
-    re_path(r"^serviceworker\.js$", service_worker, name="serviceworker"),
-    re_path(r"^manifest\.json$", manifest, name="manifest"),
+    path("serviceworker.js", service_worker, name="serviceworker"),
+    path("manifest.json", manifest, name="manifest"),
     path("offline/", offline, name="offline"),
 ]
